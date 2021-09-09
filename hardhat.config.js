3219629fc5ffc3d413f8a82bdf615867b95e1682
Victorvikson1996 <53367180+Victorvikson1996@users.noreply.github.com>
2021-09-09 17:28:06 +0530

    NFT contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,26 @@
 require("@nomiclabs/hardhat-waffle");
 
 
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "0f44cdad5f534c5591a1e17d01031269"
+
 
 
 module.exports = {
 
   networks: {
-    hardhat: {},
-    mumbai: {},
-    mainnet: {},
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
   },
   solidity: "0.8.4",
 };
